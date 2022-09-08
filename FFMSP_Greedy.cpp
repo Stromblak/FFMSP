5f67b1fda9c8f0b74989f107f9c710e3974ed4c4
Stromblak <105810066+Stromblak@users.noreply.github.com>
2022-09-08 17:46:46 -0300

    Add files via upload

--- FFMSP_Greedy.cpp
+++ FFMSP_Greedy.cpp
@@ -6,22 +6,29 @@
 #include <map>
 #include <random>
 #include <ctime>
+#include <cmath>
+
+#include <algorithm>
+
 using namespace std;
 
 
 int greedy(vector<string> &setGen, double th, int n, int m);
+int greedy_aleatorizado(vector<string> &setGen, double th, int n, int m, double a);
+
 
 int main(int argc, char *argv[]){
 	string instancia;
-	double threshold;
+	double threshold, alpha;
 	
 	srand(time(NULL));
 
 	for(int i=0; i<argc; i++){
 		if( !strcmp(argv[i], "-i" ) ) instancia = argv[i+1];
 		if( !strcmp(argv[i], "-th") ) threshold = atof(argv[i+1]);
-	}
+		if( !strcmp(argv[i], "-a") ) alpha = atof(argv[i+1]);
 
+	}
 	ifstream archivo(instancia);
 	vector<string> set;
 	string gen;
@@ -32,9 +39,17 @@ int main(int argc, char *argv[]){
 	int n = set.size();
 	int m = set[0].size();
 
-	int solucion = greedy(set, threshold, n, m);
+	double promedio = 0, promedioRandom = 0;
 
-	cout << solucion << endl;
+	double iter = 500;
+	for(int i=0; i<iter; i++){
+		promedio += greedy(set, threshold, n, m)/iter;
+		promedioRandom += greedy_aleatorizado(set, threshold, n, m, alpha)/iter;
+	}
+
+	cout << "Promedio: " << promedio << endl;
+	cout << "Promedio Random: " << promedioRandom << endl;
+
 	return 0;
 }
 
@@ -43,13 +58,30 @@ int greedy(vector<string> &setGen, double th, int n, int m){
 	vector<char> bases = {'A', 'C', 'G', 'T'};
 	vector<int> hamming(n);
 	map<char, int> cumpleTH; // contador cantidad strings donde porcentaje hamming >= th
-	string sol;
+	string sol(m,'R');
 
 	// contar cuanto se repiten las bases en cada columna
 	vector<map<char, int>> contador(m);
 	for(int col=0; col<m; col++) for(int j=0; j<n; j++) contador[col][ setGen[j][col] ]++;
 
-	for(int col=0; col<m; col++){  //para cada columna	
+
+	int contadorRepMaxima = 0;
+	vector<pair<int, int>> indices;
+	for(auto mapa: contador){
+		int baseMax = -1;
+		for(auto par: mapa) baseMax = min(par.second, baseMax);
+		indices.push_back( make_pair(baseMax, contadorRepMaxima) );
+		contadorRepMaxima++;
+	}
+
+	sort(indices.begin(), indices.end());
+	reverse(indices.begin(), indices.end());
+
+	int columnas = 0;
+	for(auto par: indices){  //para cada columna	
+		int col = par.second;
+		columnas++;
+
 		cumpleTH['A'] = 0;
 		cumpleTH['C'] = 0;
 		cumpleTH['G'] = 0;
@@ -58,8 +90,8 @@ int greedy(vector<string> &setGen, double th, int n, int m){
 		for(char base: bases){	 // para cada base
 			for(int j=0; j<n; j++){	 //para cada gen
 				double porcentajeDif;
-				if(setGen[j][col] != base) porcentajeDif = (double)(hamming[j] + 1) / (col+1);
-				else porcentajeDif = (double)(hamming[j]) / (col+1);
+				if(setGen[j][col] != base) porcentajeDif = (double)(hamming[j] + 1) / (columnas);
+				else porcentajeDif = (double)(hamming[j]) / (columnas);
 
 				if(porcentajeDif >= th) cumpleTH[base]++;
 			}
@@ -67,10 +99,10 @@ int greedy(vector<string> &setGen, double th, int n, int m){
 
 		// encontrar a los maximos de cumpleTH
 		int bMAx = -1;
-		for(pair<char, int> p: cumpleTH) bMAx = max(bMAx, p.second);
+		for(auto p: cumpleTH) bMAx = max(bMAx, p.second);
 
 		vector<char> maximos;
-		for(pair<char, int> p: cumpleTH) if(p.second == bMAx) maximos.push_back(p.first);
+		for(auto p: cumpleTH) if(p.second == bMAx) maximos.push_back(p.first);
 
 		// encontrar cuantos se repiten los maximos en la columna, eligir los minimos
 		int bMin = n+1;
@@ -83,7 +115,82 @@ int greedy(vector<string> &setGen, double th, int n, int m){
 		char actual = minRepeticion[ rand() % minRepeticion.size() ];
 
 		for(int j=0; j<n; j++) if(setGen[j][col] != actual) hamming[j]++;	
-		sol += actual;
+		sol[col] = actual;
+	}
+
+	int calidad = 0;
+	for(int h: hamming) if( (double)h / m  >= th ) calidad++;
+
+	return calidad;
+}
+
+int greedy_aleatorizado(vector<string> &setGen, double th, int n, int m, double a){
+	vector<char> bases = {'A', 'C', 'G', 'T'};
+	vector<int> hamming(n);
+	map<char, int> cumpleTH; // contador cantidad strings donde porcentaje hamming >= th
+	string sol(m,'R');
+
+	// contar cuanto se repiten las bases en cada columna
+	vector<map<char, int>> contador(m);
+	for(int col=0; col<m; col++) for(int j=0; j<n; j++) contador[col][ setGen[j][col] ]++;
+
+
+	int contadorRepMaxima = 0;
+	vector<pair<int, int>> indices;
+	for(auto mapa: contador){
+		int baseMax = -1;
+		for(auto par: mapa) baseMax = max(par.second, baseMax);
+		indices.push_back( make_pair(baseMax, contadorRepMaxima) );
+		contadorRepMaxima++;
+	}
+
+	sort(indices.begin(), indices.end());
+	reverse(indices.begin(), indices.end());
+
+	int columnas = 0;
+	for(auto par: indices){  //para cada columna	
+		int col = par.second;
+		columnas++;
+
+		cumpleTH['A'] = 0;
+		cumpleTH['C'] = 0;
+		cumpleTH['G'] = 0;
+		cumpleTH['T'] = 0;
+
+		for(char base: bases){	 // para cada base
+			for(int j=0; j<n; j++){	 //para cada gen
+				double porcentajeDif;
+				if(setGen[j][col] != base) porcentajeDif = (double)(hamming[j] + 1) / (columnas);
+				else porcentajeDif = (double)(hamming[j]) / (columnas);
+
+				if(porcentajeDif >= th) cumpleTH[base]++;
+			}
+		}
+
+
+		char actual;
+		if( (rand()%101)/100.f >= a ){
+			// encontrar a los maximos de cumpleTH
+			int bMAx = -1;
+			for(auto p: cumpleTH) bMAx = max(bMAx, p.second);
+
+			vector<char> maximos;
+			for(auto p: cumpleTH) if(p.second == bMAx) maximos.push_back(p.first);
+
+			// encontrar cuantos se repiten los maximos en la columna, eligir los minimos
+			int bMin = n+1;
+			for(char c: maximos) bMin = min(bMin, contador[col][c]);
+
+			vector<char> minRepeticion;
+			for(char c: maximos) if(contador[col][c] == bMin) minRepeticion.push_back(c);
+
+			actual = minRepeticion[ rand() % minRepeticion.size() ];
+		}else actual = bases[rand()%4];
+
+
+		for(int j=0; j<n; j++) if(setGen[j][col] != actual) hamming[j]++;	
+		sol[col] = actual;
+
 	}
 
 	int calidad = 0;
