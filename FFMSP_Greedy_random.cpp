5b1c9874d1b3c956bc79e128d8f23f2b6f330ffa
Stromblak <105810066+Stromblak@users.noreply.github.com>
2022-09-10 12:24:39 -0300

    Add files via upload

--- FFMSP_Greedy_random.cpp
+++ FFMSP_Greedy_random.cpp
@@ -59,36 +59,35 @@ int greedy_random(vector<string> &setGen, double th, int n, int m, double a){
 	for(auto par: indices){  // para cada columna	
 		int col = par.second;
 		columnasListas++;
-		
+		cumpleTH['A'] = 0;
+		cumpleTH['C'] = 0;
+		cumpleTH['G'] = 0;
+		cumpleTH['T'] = 0;
+
 		if(rand()%101 <= a*100){
 			sol[col] = bases[rand()%4];
 			for(int j=0; j<n; j++) if(setGen[j][col] != sol[col]) hamming[j]++;	
 			continue;
 		}
 		
-		cumpleTH['A'] = 0;
-		cumpleTH['C'] = 0;
-		cumpleTH['G'] = 0;
-		cumpleTH['T'] = 0;
-
 		for(char base: bases){	 // para cada base a testear
 			for(int j=0; j<n; j++){	 //para cada base en la columna
-				int hammingActual = hamming[j];
-				if(setGen[j][col] != base) hammingActual++;
-				if(hammingActual >= th*columnasListas) cumpleTH[base]++;
+				if(hamming[j] + 1*(setGen[j][col] != base) >= th*columnasListas){
+					cumpleTH[base]++;
+				}
 			}
 		}
 
 		// encontrar a los maximos de cumpleTH
 		int cumpleThMAx = -1;
-		for(auto par: cumpleTH) cumpleThMAx = max(cumpleThMAx, par.second);
 		vector<char> maximos;
+		for(auto par: cumpleTH) cumpleThMAx = max(cumpleThMAx, par.second);
 		for(auto par: cumpleTH) if(par.second == cumpleThMAx) maximos.push_back(par.first);
 
 		// elegir los minimos de cuanto se repiten los maximos en la columna
 		int repMin = n+1;
-		for(char c: maximos) repMin = min(repMin, contador[col][c]);
 		vector<char> minRepeticion;
+		for(char c: maximos) repMin = min(repMin, contador[col][c]);
 		for(char c: maximos) if(contador[col][c] == repMin) minRepeticion.push_back(c);
 		
 		// elegir al azar entre los minimos y actualizar dist hamming
